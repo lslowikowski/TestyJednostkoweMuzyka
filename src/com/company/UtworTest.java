235c8e53385f5e92efdbdee8c69d7e3020fe8590
Leszek Słowikowski <lslowikowski@gmail.com>
2022-01-22 14:43:35 +0100

    ver 003 - Dodanie komentarzy generujących JavaDoc w klasie testowej

--- src/com/company/UtworTest.java
+++ src/com/company/UtworTest.java
@@ -3,10 +3,16 @@ package com.company;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Klasa testująca klasę Utwór. <br>
+ * Zawiera testy jednostkowe wiekszości metod
+ */
 class UtworTest {
 
+    /**
+     * Sprawdza poprawność tytułu,
+     * czy zbyt długi {@link com.company.Utwor#tytul} jest obcinany do 30 znaków
+     */
     @Test
     void poprawnoscTytulu() {
         Utwor utwor = new Utwor("Test piosenki", 5, 20);
@@ -14,6 +20,10 @@ class UtworTest {
         Assertions.assertEquals("Tytul1234_Tytul1234_Tytul1234_", utwor.getTytul());
     }
 
+    /**
+     * Sprawdza poprawność wykonawcy,
+     * czy zbyt długi {@link com.company.Utwor#wykonawca} jest obcinany do 20 znaków
+     */
     @Test
     void poprawnoscWykonawcy() {
         Utwor utwor = new Utwor("Test piosenki", 5, 20);
@@ -21,6 +31,10 @@ class UtworTest {
         Assertions.assertEquals("Wykonawca_Wykonawca_", utwor.getWykonawca());
     }
 
+    /**
+     * Sprawdza poprawność czasu,
+     * czy ujemny {@link com.company.Utwor#czas} jest zamieniany na 0
+     */
     @Test
     void poprawnyCzas() {
         Utwor utwor = new Utwor("Test piosenki", 5, 20);
@@ -28,6 +42,11 @@ class UtworTest {
         Assertions.assertEquals(0, utwor.getCzas());
     }
 
+    /**
+     * Sprawdza poprawność konstruktora,
+     * czy zbyt długi {@link com.company.Utwor#tytul} jest obcinany do 30 znaków<br>
+     * czy ujemny {@link com.company.Utwor#czas} jest zamieniany na 0
+     */
     @Test
     void poprawnyKonstruktor() {
         Utwor utwor = new Utwor("Tytul1234_Tytul1234_Tytul1234_abc", -5, 20);
@@ -36,6 +55,10 @@ class UtworTest {
         Assertions.assertEquals("nieznany", utwor.getWykonawca());
     }
 
+    /**
+     * Sprawdza poprawność czasu w formacie mm:ss
+     * czy {@link com.company.Utwor#czas} jest zamieniany 380 sekund to 6:20
+     */
     @Test
     void poprawnyCzasTrwania() {
         Utwor utwor = new Utwor("Test piosenki", 5, 20);
@@ -43,6 +66,10 @@ class UtworTest {
         Assertions.assertEquals("6:20", utwor.czasTrwania());
     }
 
+    /**
+     * Sprawdza poprawność metody toString
+     * czy {@link Utwor#toString()} zwróci poprawną wartość
+     */
     @Test
     void poprawnoscToString() {
         Utwor utwor = new Utwor("Test piosenki", 5, 20);
